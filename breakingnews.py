5a2421894d21f8234e451fcd7928798503d3de6a
emrsenturk <sentuurkemre@gmail.com>
2017-09-08 23:47:44 +0300

    Add files via upload

---
+++ breakingnews.py
@@ -0,0 +1,40 @@
+# -*- coding: cp1254 -*-
+import mechanize
+import time
+from bs4 import BeautifulSoup
+
+
+br = mechanize.Browser()
+
+br.addheaders = [("User-agent","Chrome")]
+br.set_handle_robots(False)
+
+
+id = 332502
+while True: 
+    
+    source = br.open("http://www.trthaber.com/m/?news=&news_id="+str(id)).read()
+    soup = BeautifulSoup(source)
+    writingHead= soup.h2 
+
+    
+    if  writingHead == None:
+        print "NONEWS"
+    
+    elif writingHead != None:
+        print "BREAKINGNEWS"
+        
+        contentHTML = soup.find_all("p")
+        content = ""
+       
+        for i in contentHTML:
+            content = content + i.text
+        
+        news = writingHead.text.upper()+"\n\n\n\n"+content
+        print news
+        
+        
+        
+        id = id+1
+    
+    time.sleep(30)
